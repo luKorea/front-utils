79a8af6fc3f3e9cf29ca8b00c7e8991491c66b9a
korealu <643949593@qq.com>
2022-03-28 14:50:52 +0800

    project-init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-28 14:39:14
+ * @LastEditTime: 2022-03-28 14:43:08
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /korea-utils/src/index.ts
+ */
+export { default as tools } from './tool/index'
+export { default  as deepClone } from './deepClone/index' 
